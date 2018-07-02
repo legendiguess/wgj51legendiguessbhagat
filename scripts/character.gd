0a6fda6b74c19832b8c96064b49fbdd7adec14ea
legendiguess <legendiguess@mail.ru>
2018-07-02 20:18:02 +0700

    working on character movement & character animation

--- scripts/character.gd
+++ scripts/character.gd
@@ -20,6 +20,8 @@ func _process(delta):
 			motion.z += STOP
 	###############################
 	if Input.is_action_pressed("ui_down"):
+		if !$character_spatial/character_animation.current_animation == "WalkAnim":
+			$character_spatial/character_animation.play("WalkAnim")
 		if motion.z < MAXSPEED:
 			motion.z += SPEED
 	else:
@@ -44,7 +46,8 @@ func _process(delta):
 	if motion.z<0:
 		$character_sprites.play("run_up")
 	if motion.z == 0 and motion.x == 0:
-		$character_sprites.play("idle_down")
+		if !$character_spatial/character_animation.current_animation == "Idle":
+			$character_spatial/character_animation.play("Idle")
 	move_and_slide(motion*delta)
 	print(motion)
 	pass

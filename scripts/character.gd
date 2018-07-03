cf3e92ffcf6bd9dd094cbb79421c5cd319d5a70a
BhagatN <bhagat.nagi@gmail.com>
2018-07-03 10:41:27 +0530

    Some changes to the player script

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,18 +1,19 @@
 extends KinematicBody
 
-var motion = Vector3(0,0,0)
-var MAXSPEED = 500
-var SPEED= 50
-var STOP = 50
+onready var motion = Vector3(0,0,0)
+onready var MAXSPEED = 300
+onready var SPEED= 50
+onready var STOP = 50
+onready var rot_target = 0
+onready var rot_y = 0
+onready var rot_speed = 10
 
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
 
-func _process(delta):
+func _physics_process(delta):
 	####################################################### z movement
 	if Input.is_action_pressed("ui_up"):
+		rot_target = 180
+		walk_anim()
 		if motion.z > -MAXSPEED:
 			motion.z -= SPEED
 	else:
@@ -20,8 +21,8 @@ func _process(delta):
 			motion.z += STOP
 	###############################
 	if Input.is_action_pressed("ui_down"):
-		if !$character_spatial/character_animation.current_animation == "WalkAnim":
-			$character_spatial/character_animation.play("WalkAnim")
+		rot_target = 0
+		walk_anim()
 		if motion.z < MAXSPEED:
 			motion.z += SPEED
 	else:
@@ -29,6 +30,8 @@ func _process(delta):
 			motion.z -= STOP
 	######################################################## x movement
 	if Input.is_action_pressed("ui_left"):
+		rot_target = 270
+		walk_anim()
 		if motion.x > -MAXSPEED:
 			motion.x -= SPEED
 	else:
@@ -36,18 +39,39 @@ func _process(delta):
 			motion.x += STOP
 	###############################
 	if Input.is_action_pressed("ui_right"):
+		rot_target = 90
+		walk_anim()
 		if motion.x < MAXSPEED:
 			motion.x += SPEED
 	else:
 		if motion.x > 0:
 			motion.x -= STOP
-	if motion.z>0:
-		$character_sprites.play("run_down")
-	if motion.z<0:
+	if motion.z != 0:
 		$character_sprites.play("run_up")
 	if motion.z == 0 and motion.x == 0:
 		if !$character_spatial/character_animation.current_animation == "Idle":
 			$character_spatial/character_animation.play("Idle")
+	
+	rotate_mesh()
+	
 	move_and_slide(motion*delta)
-	print(motion)
-	pass
+#	print(motion)
+
+func walk_anim():
+	if $character_spatial/character_animation.current_animation != "WalkAnim":
+			$character_spatial/character_animation.play("WalkAnim")
+
+func rotate_mesh():
+	
+#	if rot_y == 360:
+#		rot_y = 0
+#
+#	if rot_y != rot_target:
+#		if rot_y < (rot_y + 180):
+#			rot_y -= rot_speed
+#		elif rot_y >= (rot_y + 180):
+#			rot_y += rot_speed
+#		print(rot_y)
+	
+	$character_spatial.rotation_degrees.y = rot_target
+

19a0779cecc264d9ac9ef862a94472856d7cdf4b
legendiguess <legendiguess@mail.ru>
2018-07-03 13:23:38 +0700

    added hp bar and readd lowpoly mat

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,9 +1,10 @@
 extends KinematicBody
 
 var motion = Vector3(0,0,0)
-var MAXSPEED = 500
-var SPEED= 50
-var STOP = 50
+var MAXSPEED = 350
+var SPEED= 35
+var HP = 100
+var ROT = 0.0
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -17,30 +18,32 @@ func _process(delta):
 			motion.z -= SPEED
 	else:
 		if motion.z < -0:
-			motion.z += STOP
+			motion.z += SPEED
 	###############################
 	if Input.is_action_pressed("ui_down"):
+		HP -=1
 		if !$character_spatial/character_animation.current_animation == "WalkAnim":
 			$character_spatial/character_animation.play("WalkAnim")
 		if motion.z < MAXSPEED:
 			motion.z += SPEED
 	else:
 		if motion.z > 0:
-			motion.z -= STOP
+			motion.z -= SPEED
 	######################################################## x movement
 	if Input.is_action_pressed("ui_left"):
 		if motion.x > -MAXSPEED:
 			motion.x -= SPEED
 	else:
-		if motion.x < 0:
-			motion.x += STOP
+		if motion.x < -0:
+			motion.x += SPEED
 	###############################
 	if Input.is_action_pressed("ui_right"):
 		if motion.x < MAXSPEED:
 			motion.x += SPEED
 	else:
 		if motion.x > 0:
-			motion.x -= STOP
+			motion.x -= SPEED
+	
 	if motion.z>0:
 		$character_sprites.play("run_down")
 	if motion.z<0:
@@ -48,6 +51,8 @@ func _process(delta):
 	if motion.z == 0 and motion.x == 0:
 		if !$character_spatial/character_animation.current_animation == "Idle":
 			$character_spatial/character_animation.play("Idle")
-	move_and_slide(motion*delta)
+	motion = motion.rotated(Vector3(0, 1, 0), ROT)
+	move_and_slide(motion*delta, Vector3(0, 0, 1))
+	get_node("../gui/health_bar").value = HP
 	print(motion)
 	pass
